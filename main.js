8cd33d01877fa294134f7da19723e9f2cf97a953
vincentkurniawan <6181901024@student.unpar.ac.id>
2022-08-18 08:26:04 +0700

    fix date

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "08/18/ 10:00:00",
+        dayMonth = "08/18/",
         birthday = dayMonth + yyyy;
 
     today = mm + "/" + dd + "/" + yyyy;
